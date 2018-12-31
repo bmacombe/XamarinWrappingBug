bd672e1554523c1bf00274e08bdf01d3218c688f
John Roemer <john@ecp123.com>
2018-12-31 12:39:56 -0600

    init

---
+++ WrappingBug/WrappingBug/WrappingBug/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using WrappingBug.Models;
+
+namespace WrappingBug.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
